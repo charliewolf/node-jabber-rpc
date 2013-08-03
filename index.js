dc34fb67a7fd13b1c501339ec463640ba33e2663
Charlie Wolf <charlie@wolf.is>
2013-08-02 20:37:08 -0700

    Adding better documentation

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ var jabberrpc = exports
 
 /**
  * Creates an Jabber-RPC client.
+ * Takes same options parameters as node-xmpp.Client
  *
- * @param {Object} options - server options to make the HTTP request to
- *   - {String} host
- *   - {Number} port
- *   - {String} url
- *   - {Boolean} cookies
- * @return {Client}
- * @see Client
+ *   jid: String (required)
+ *   password: String (required)
+ *   host: String (optional)
+ *   port: Number (optional)
+ *   reconnect: Boolean (optional)
+ *   register: Boolean (option) - register account before authentication
+ *   legacySSL: Boolean (optional) - connect to the legacy SSL port, requires at least the host to be specified
+ *   credentials: Dictionary (optional) - TLS or SSL key and certificate credentials
+ *   actAs: String (optional) - if admin user act on behalf of another user (just user)
+ *
  */
+
 jabberrpc.createConnection = function(options) {
 return new Connection(options);
 };
