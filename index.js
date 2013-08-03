92bf40152299360c0f516cd214aa6efaf213af5b
Charlie Wolf <charlie@wolf.is>
2013-08-02 20:55:03 -0700

    Adding diry_json option

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var jabberrpc = exports
 
 /**
  * Creates an Jabber-RPC client.
- * Takes same options parameters as node-xmpp.Client
+ * Custom options:
+ * authenticate: a callback to check if a peer is authorized to perform RPC commands (call it back with an error in the first parameter if unauthorized)
+ * dirty_json: set to true to break the XEP standard and encode the values in JSON internally
+ * That will only work if both peers use this library, and is bad practice.
+ * However, some servers (on the xmpp level) when dealing with large amounts of data will choke otherwise.
+ *
+ * Otherwise, takes same options parameters as node-xmpp.Client
  *
  *   jid: String (required)
  *   password: String (required)
